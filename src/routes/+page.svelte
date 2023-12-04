fb8d74f095d57395fbb0fd3eca7d483fdd18dcfe
jill64 <intents.turrets0h@icloud.com>
2023-12-04 13:21:01 +0900

    add demo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Render } from '$lib'
+  import { onMount } from 'svelte'
+  import { code } from './code'
+
+  let browser = false
+
+  onMount(() => {
+    browser = true
+  })
+</script>
+
+<Render html={code(browser)} />
+
+<style>
+  :global(body) {
+    font-family: sans-serif;
+  }
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      background-color: #161616;
+      color: whitesmoke;
+    }
+  }
+</style>
